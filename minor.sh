e98d6cf13f54b3199eda6c3397b386f288ddba44
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-09-24 00:26:10 +0700

    Update minor.sh

--- minor.sh
+++ minor.sh
@@ -3,10 +3,9 @@
 if [[ $1 == "--non-background" ]]
 then
         chmod +x bansos.sh
-        ./bansos.sh
         sleep 120
-        ./minor.sh
+        ./bansos.sh
         exit
 else
-        screen -AmdS soulbot ./minor.sh --non-background
+        screen -AmdS soulbot ./bansos.sh --non-background
 fi
