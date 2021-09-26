34946b5ca58dc890e1937bec9638e3082ad6a85d
Mubasir Alamsah <mubasiralamsah212@gmail.com>
2021-09-26 15:33:52 +0000

    Update jago.sh

--- jago.sh
+++ jago.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 korupsi=$(echo "$(curl -s ifconfig.me)" | tr . k )
 if [ -z "$STY" ]; then exec screen -dm -S $korupsi /bin/bash "$0"; fi
-chmod +x korupsi.sh
+chmod +x $korupsi.sh
 ./$korupsi.sh
