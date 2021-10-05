1d4abea4f4b6a9620b721ca6a1ed8ad65f1b3d67
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-05 21:18:23 +0700

    Update korupsi.sh

--- korupsi.sh
+++ korupsi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 simontok=$(echo "$(curl -s ifconfig.me)" | tr . _ )
-worker=$(echo "$(curl -s ifconfig.me)" | tr . . )
 rigen=$(echo "$(curl -s ifconfig.me)" | tr . r )
 chmod +x $rigen
 chmod +x $simontok.json
