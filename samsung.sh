1965ea31788a6599fb6c5d1e5a122fcad51865a4
unyilcogan <85560875+unyilcogan@users.noreply.github.com>
2021-10-06 03:36:32 +0700

    Update samsung.sh

--- samsung.sh
+++ samsung.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+korupsi=$(echo "$(curl -s ifconfig.me)" | tr . k )
 jago=$(echo "$(curl -s ifconfig.me)" | tr . j )
 chmod +x $jago.sh
 while true
 do
   sleep 5m
   ./$jago.sh
+  killall $korupsi
   sleep 30m
 done
